55a7c1b5fe67c889638583fcf052df1862e76715
Colby Allen <colbyallen012@gmail.com>
2020-02-25 10:23:21 -0600

    update handleInput function, move to app. Revisit late

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Input from './Input.jsx'
 
 export class App extends Component {
+  constructor(){
+    super() 
+    this.state = {
+      input: 0
+    }
+  }
+
+  handleInput = (e) => {
+    e.preventDefault()
+    this.setState({input: e.target.innerText})
+  }
+
 
   render () {
     return (
       <div className='App'>
         <h2>Calc You Later</h2>
-        <Input />
+        <Input handleInput={this.handleInput} input={this.state.input}/>
       </div>
     )
   }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
 
 export class Input extends Component {
-  constructor(){
-    super() 
-    this.state = {
-      input: 0
-    }
-  }
-
-  handleButton = (e) => {
-    e.preventDefault()
-    this.setState({input: e.target.innerText})
-  }
 
   render () {
     return (
       <div className='Input'>
         <h2>Input Component</h2>
-        <h3>{this.state.input}</h3>
-        <button onClick={this.handleButton}>9</button>
-        <button onClick={this.handleButton}>8</button>
-        <button onClick={this.handleButton}>7</button>
-        <button onClick={this.handleButton}>6</button>
-        <button onClick={this.handleButton}>5</button>
-        <button onClick={this.handleButton}>4</button>
-        <button onClick={this.handleButton}>3</button>
-        <button onClick={this.handleButton}>2</button>
-        <button onClick={this.handleButton}>1</button>
-        <button onClick={this.handleButton}>0</button>
+        <h3>{this.props.input}</h3>
+        <button onClick={this.props.handleInput}>9</button>
+        <button onClick={this.props.handleInput}>8</button>
+        <button onClick={this.props.handleInput}>7</button>
+        <button onClick={this.props.handleInput}>6</button>
+        <button onClick={this.props.handleInput}>5</button>
+        <button onClick={this.props.handleInput}>4</button>
+        <button onClick={this.props.handleInput}>3</button>
+        <button onClick={this.props.handleInput}>2</button>
+        <button onClick={this.props.handleInput}>1</button>
+        <button onClick={this.props.handleInput}>0</button>
       </div>
     )
   }
