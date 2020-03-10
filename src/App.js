ba77e6210c4d3d3c66a7791a6c6f64e01f0c3616
Colby Allen <colbyallen012@gmail.com>
2020-03-10 10:01:52 -0600

    start refactor on input function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Input from './Input.jsx'
+import Operator from './Operator.jsx'
 
 export class App extends Component {
   constructor(){
     super() 
     this.state = {
-      input: 0
+      input: 0,
+      operator: null
     }
   }
 
   handleInput = (e) => {
     e.preventDefault()
-    this.setState({input: e.target.innerText})
+
+    if (e.target.innerText === '+'){
+      console.log('plus')
+    } else {
+      this.setState({input: e.target.innerText})
+    }
+  }
+
+  handleOperator = (e) => {
+    e.preventDefault()
+    this.setState({operator: e.target.innerText})
+    console.log(this.state)
   }
 
 
@@ -20,7 +33,9 @@ export class App extends Component {
     return (
       <div className='App'>
         <h2>Calc You Later</h2>
-        <Input handleInput={this.handleInput} input={this.state.input}/>
+        <h3>{this.state.input}</h3>
+        <Input handleInput={this.handleInput} />
+        <Operator handleOperator={this.handleOperator} />
       </div>
     )
   }
